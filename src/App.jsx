94c7aba09b8c2c3ff5b47d77dbd17eaebc0d7c95
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-04-01 16:59:47 +0000

    style app

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ function App() {
   return (
     <div>
       <Header />
+      <main>
       {cards}
+      </main>
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -8,21 +8,21 @@ export default function Card(props) {
         />
         <div className="card--details">
           <p>
-            <img src="https://img.icons8.com/ios-filled/50/FD7E14/address--v1.png" />
+            <img src="https://img.icons8.com/ios-filled/50/FD7E14/address--v1.png" className="card--locationLogo"/>
             <span className="card--location">{props.item.location}</span>
-            <a className="card--link" href={props.item.googleMapsUrl}>
+            <a className="card--link" href={props.item.googleMapsUrl} target="_blank">
               View on Google Maps
             </a>
           </p>
           <h3 className="card--title">{props.item.title}</h3>
-          <p className="card--dates">
+          <b className="card--dates">
             <span className="card--startDate">{props.item.startDate}</span>
             <span className="card--endDate">{props.item.endDate}</span>
-          </p>
+          </b>
           <p className="card--description">{props.item.description} </p>
         </div>
       </div>
-      <hr />
+      {props.item.id !== 3 && <hr /> } 
     </>
   );
 }
