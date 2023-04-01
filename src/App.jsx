2da2e5d04231efbda128af2aae7a6ec8b0278301
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-04-01 16:25:18 +0000

    build card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import Header from "./components/header";
+import Card from "./components/card";
+import data from "./data";
 
 function App() {
+  let cards = data.map( item => {
+    return (
+      <Card 
+        key={item.id}
+        item= {item}
+      />
+    )
+  })
   return (
-    <>
+    <div>
       <Header />
-    </>
+      {cards}
+    </div>
   );
 }
 
---
+++ src/components/card.jsx
@@ -0,0 +1,21 @@
+export default function Card(props) {
+  return(
+    <div className="card">
+        <img src={`../../public/${props.item.imageUrl}`} className="card--img" />
+        <div className="card--details">
+          <p>
+            <img src="https://img.icons8.com/ios-filled/50/FD7E14/address--v1.png"/>  
+            <span className="card--location">{props.item.location}</span>
+            <a className="card--link" href={props.item.googleMapsUrl}>View on Google Maps</a>
+          </p>
+          <h3 className="card--title">{props.item.title}</h3>
+          <p className="card--dates">
+            <span className="card--startDate">{props.item.startDate}</span>
+            <span className="card--endDate">{props.item.endDate}</span>
+          </p>
+          <p className="card--description">{props.item.description} </p>
+        </div>
+    </div>
+
+  )
+}
